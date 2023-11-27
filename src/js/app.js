b66597c177f2a5e86c48600d7e8c4e674ef2db4e
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-11-28 01:09:10 +0630

    add & minus btn finished

--- src/js/app.js
+++ src/js/app.js
@@ -81,7 +81,7 @@ const recordUI = (productName, price, quantity) => {
   const tr = document.createElement("tr");
 
   tr.className =
-    "odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 group ";
+    "odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 group relative ";
   tr.innerHTML = `
     <td class="px-6 py-4 text-left td-counter" ></td>
     <th
@@ -91,12 +91,25 @@ const recordUI = (productName, price, quantity) => {
      ${productName}
     </th>
 
-    <td class="px-6 py-4 text-end">${price}</td>
-    <td class="px-6 py-4 text-end">${quantity}</td>
+    <td class="px-6 py-4 record-price">${price}</td>
+    <td class="px-6 py-4  text-sm">
+      <button class="sub-q bg-blue-100 rounded-md p-1 pointer-events-none group-hover:pointer-events-auto -translate-x-2 group-hover:translate-x-0 duration-200 opacity-0 group-hover:opacity-100 active:scale-75">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 stroke-blue-600 pointer-events-none ">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M18 12H6" />
+        </svg>
+      </button>
+      <span class=" w-5 inline-block text-center record-q">${quantity}</span>
+      <button class="add-q bg-blue-100 rounded-md p-1 pointer-events-none group-hover:pointer-events-auto translate-x-2 group-hover:translate-x-0 duration-200 opacity-0 group-hover:opacity-100 active:scale-75">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 stroke-blue-600 pointer-events-none ">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m6-6H6" />
+        </svg>     
+      </button>
 
-    <td class="px-2 py-4 text-end relative">
+    </td>
+
+    <td class="px-6 py-4  relative">
       <span class="record-cost">${cost}</span>
-      <button class="record-del bg-gray-200 rounded-md  translate-x-4 group-hover:translate-x-2 opacity-0 group-hover:opacity-100 duration-200 p-1 fixed active:scale-75 pointer-events-auto">
+      <button class="record-del bg-gray-200 rounded-md absolute right-2 opacity-0 group-hover:opacity-100 translate-x-2 group-hover:translate-x-0 duration-200 p-1  active:scale-75 pointer-events-auto">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 stroke-1 stroke-red-800 pointer-events-none">
       <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
     </svg>
@@ -163,8 +176,31 @@ const recordGroupHandler = (event) => {
   //   console.log(x);
   if (event.target.classList.contains("record-del")) {
     console.log(event.target.closest("tr").remove());
+    calRecordTotal();
+  }else if(event.target.classList.contains("add-q")){
+    // console.log(event.target);
+    const currentRow = event.target.closest("tr");
+    const currentRowQ = currentRow.querySelector(".record-q");
+    const currentRowCost = currentRow.querySelector(".record-cost");
+    const currentRowPrice = currentRow.querySelector(".record-price");
+
+    currentRowQ.innerText = parseInt(currentRowQ.innerText) + 1;
+    currentRowCost.innerText = currentRowPrice.innerText * currentRowQ.innerText;
+    calRecordTotal();
+    
+  }else if(event.target.classList.contains("sub-q")){
+    // console.log(event.target);
+    const currentRow = event.target.closest("tr");
+    const currentRowQ = currentRow.querySelector(".record-q");
+    const currentRowCost = currentRow.querySelector(".record-cost");
+    const currentRowPrice = currentRow.querySelector(".record-price");
+
+    currentRowQ.innerText = parseInt(currentRowQ.innerText) - 1;
+    currentRowCost.innerText = currentRowPrice.innerText * currentRowQ.innerText;
+    calRecordTotal();
+    
   }
-  calRecordTotal();
+  
 };
 
 
